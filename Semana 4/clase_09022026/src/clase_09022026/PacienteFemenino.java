b43769cd8fd28e97eed529be8e109a42570b7d67
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2026-02-09 19:54:31 -0600

    clase 09 de febrero: herencia

---
+++ Semana 4/clase_09022026/src/clase_09022026/PacienteFemenino.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_09022026;
+
+/**
+ *
+ * @author claudiacortes
+ */
+
+/*
+La configuracion de la herencia, siempre se hace de lado del hijo 
+*/
+public class PacienteFemenino extends Paciente{
+    boolean embarazo;
+    String ultimoPeriodoMenstrual;
+
+    
+}
